3a612883f30a7a5606c5b15b7da23328553e6381
Daiki Fujimoto <dai.fuji.110728@gmail.com>
2021-11-14 23:29:51 +0900

    pre-finish

--- js/main.js
+++ js/main.js
@@ -1,38 +1,24 @@
 console.log("js正常読み込み");
 $(function(){
-    $(".c-close").click(function(){
+    $(".c-button-close").click(function(){
         $(".p-sidebar").removeClass("is-open");
         $(".c-background").removeClass("is-open");
-        $("main").removeClass("is-fixed");
+        $("body").css("overflow","unset");
+
     });
     
     $(".p-header__menu").click(function(){
         $(".p-sidebar").addClass("is-open");
         $(".c-background").addClass("is-open");
-        $("main").addClass("is-fixed");
+        $("body").css("overflow","hidden");
     });
 
-
-    if (window.matchMedia('(max-width: 1200px)').matches) {
-        $(".has-submenu").hover(function(){//サブメニューの表示非表示
-            console.log("ホバーを検出");
-            $(this).children("ul").stop(true).slideDown();
-        },function(){
-            $(this).children("ul").stop(true).slideUp();
-    });}
-
-
-    
     $(window).resize(function(){
         let currentWidth = $(window).width();                
-        $(".gnavi-opened").hide();
-        $(".drawermenu").removeClass("opened");
-        console.log("ハンバーガーメニュー非表示");
-
-        if(currentWidth>1200){
-            $(".gnavi-opened").show();
-            console.log("ヘッダーメニュー表示")
-        }
+        $(".p-sidebar").removeClass("is-open");
+        $(".c-background").removeClass("is-open");
+        $("body").css("overflow","unset");
+        console.log("メニュー非表示");
         
     });
 
